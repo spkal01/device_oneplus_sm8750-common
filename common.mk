702ac7602755fc909e08dc31628caead127cba4f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-01-28 21:14:48 +0530

    sm8750-common: Silence some spammy logging
    OplusTouch, iris, and bt loggers (particularly when find my device
    is enabled) are severe.
    
    Change-Id: I1637c6147c26507cab851459251396668ed67ec8
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- common.mk
+++ common.mk
@@ -283,6 +283,25 @@ PRODUCT_PACKAGES += \
 
 $(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
 
+# Logging
+SPAMMY_LOG_TAGS := \
+    OplusTouchDaemon \
+    vendor.qti.bluetooth@1.1-wake_lock \
+    vendor.qti.bluetooth@1.1-ibs_handler \
+    IRIS_LOG_SERV_I7 \
+    SDM \
+    synaLib \
+    CwbService \
+    occe_create \
+    DisplayModeController \
+    StatsLog \
+    vendor.qti.vibrator
+
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+PRODUCT_VENDOR_PROPERTIES += \
+    $(foreach tag,$(SPAMMY_LOG_TAGS),log.tag.$(tag)=S)
+endif
+
 # Media
 PRODUCT_COPY_FILES += \
     $(AUDIO_HAL_DIR)/configs/common/codec2/media_codecs_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2_audio.xml \
